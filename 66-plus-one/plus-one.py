5b60d3ec56840fc910e98796021cbd535012cddd
Nathan Ng <nathanuyng898@gmail.com>
2026-03-22 20:21:44 +0800

    Time: 0 ms (100.00%) | Memory: 12.4 MB (42.33%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+
+        return [1] + digits
+
+        
